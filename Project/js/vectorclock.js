a3d50acd88b1093dfc8013958802b1a53dff458b
Server <red17electro@gmail.com>
2018-06-26 17:06:12 +0200

    fix: update thesis structure; impl: add crdt implementation (temp ones);

---
+++ Project/js/vectorclock.js
@@ -0,0 +1,43 @@
+class vectorclock {
+    constructor(size) {
+        this.v = [];
+
+        for (let i = 0; i < size; i++) {
+            this.v.push(0);
+        }
+    }
+
+    get(r) {
+        return this.v[r];
+    }
+
+    with(r, n) {
+        let res = new vectorclock(this.v.length);
+        for (let i = 0; i < this.v.length; i++) {
+            res.v[i] = this.v[i];
+        }
+        res.v[r] = n;
+        return res;
+    }
+
+    leq(v) {
+        for (let i = 0; i < this.v.length; i++) {
+            if (this.v[i] > v.get(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    merge(v) {
+        let res = new Vectorclock(this.v.length);
+        for (let i = 0; i < this.v.length; i++) {
+            res.v[i] = Math.max(this.get(i), v.get(i))
+        }
+        return res;
+    }
+
+    toString() {
+        return "VC" + this.v.toString();
+    }
+}
