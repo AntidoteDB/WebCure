376b383f4cfa8d43bb5fee235a945b0cd32d3b1a
Server <red17electro@gmail.com>
2018-09-12 03:40:19 +0200

    feat: add tests for the rest of functionality;

--- Project/spec/SetOfflineSpec.js
+++ Project/spec/SetOfflineSpec.js
@@ -29,4 +29,112 @@ describe('Set Offline', function() {
       });
     });
   });
+
+  it('Get set, add element to it and check changes, check the value by old timestamp, apply some changes on that one, check lastest changes [h]', function(done) {
+    let timestamp, newTimestamp;
+    let item;
+    const key = 'l';
+
+    TestHelper.checkGet(type, key, [], function(result) {
+      item = new SetCRDT(key, result.cont);
+      timestamp = result.lastCommitTimestamp;
+
+      TestHelper.checkPut(type, key, { value: 'a' }, function() {
+        TestHelper.checkGet(type, key, ['a'], function() {
+          TestHelper.checkPut(
+            type,
+            key + '/timestamp',
+            {
+              timestamp: timestamp
+            },
+            function(result) {
+              expect(result.cont).toEqual(item.calculateState());
+              expect(result.lastCommitTimestamp).toEqual(timestamp);
+              TestHelper.checkPut(
+                type,
+                key,
+                { lastCommitTimestamp: { data: timestamp }, value: 'b' },
+                function(result) {
+                  newTimestamp = result.lastCommitTimestamp;
+                  TestHelper.checkPut(type, key + '/timestamp', { timestamp: timestamp }, function(
+                    result
+                  ) {
+                    expect(result.cont).toEqual(item.calculateState());
+                    expect(result.lastCommitTimestamp).toEqual(timestamp);
+                    TestHelper.checkPut(
+                      type,
+                      key + '/timestamp',
+                      { timestamp: newTimestamp },
+                      function(result) {
+                        expect(result.lastCommitTimestamp).toEqual(newTimestamp);
+                        TestHelper.checkGet(type, key, ['a', 'b'], done);
+                      }
+                    );
+                  });
+                }
+              );
+            }
+          );
+        });
+      });
+    });
+  });
+
+  it('Get set, remove elements from it and check changes, check the value by old timestamp, apply some changes on that one, check lastest changes [h]', function(done) {
+    let timestamp, newTimestamp;
+    let item;
+    const key = 'm';
+
+    TestHelper.checkGet(type, key, [], function() {
+      TestHelper.checkPut(type, key, { value: 'a' }, function() {
+        TestHelper.checkPut(type, key, { value: 'b' }, function() {
+          TestHelper.checkGet(type, key, ['a', 'b'], function(result) {
+            item = new SetCRDT(key, result.cont);
+            timestamp = result.lastCommitTimestamp;
+            TestHelper.checkDel(type, key, { value: 'a' }, function() {
+              TestHelper.checkGet(type, key, ['b'], function() {
+                TestHelper.checkPut(
+                  type,
+                  key + '/timestamp',
+                  {
+                    timestamp: timestamp
+                  },
+                  function(result) {
+                    expect(result.cont).toEqual(item.calculateState());
+                    expect(result.lastCommitTimestamp).toEqual(timestamp);
+                    TestHelper.checkDel(
+                      type,
+                      key,
+                      { lastCommitTimestamp: { data: timestamp }, value: 'b' },
+                      function(result) {
+                        newTimestamp = result.lastCommitTimestamp;
+                        TestHelper.checkPut(
+                          type,
+                          key + '/timestamp',
+                          { timestamp: timestamp },
+                          function(result) {
+                            expect(result.cont).toEqual(item.calculateState());
+                            expect(result.lastCommitTimestamp).toEqual(timestamp);
+                            TestHelper.checkPut(
+                              type,
+                              key + '/timestamp',
+                              { timestamp: newTimestamp },
+                              function(result) {
+                                expect(result.lastCommitTimestamp).toEqual(newTimestamp);
+                                TestHelper.checkGet(type, key, [], done);
+                              }
+                            );
+                          }
+                        );
+                      }
+                    );
+                  }
+                );
+              });
+            });
+          });
+        });
+      });
+    });
+  });
 });
