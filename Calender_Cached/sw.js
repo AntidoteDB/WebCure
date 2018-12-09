0b56d0d2934560e34fb1170360e662ac3226e32a
Server <red17electro@gmail.com>
2018-12-09 18:45:59 +0100

    fix bugs in the service worker;

--- Calender_Cached/sw.js
+++ Calender_Cached/sw.js
@@ -63,21 +63,11 @@ self.addEventListener("sync", function(event) {
 });
 
 function pushUserChangesToTheServer() {
-  debugger;
-  if (
-    typeof idb === "undefined" ||
-    typeof DBHelper === "undefined" ||
-    typeof SetCRDT === "undefined"
-  ) {
-    self.importScripts("js/dbhelper.js", "js/idb.js", "js/CRDTs/SetCRDT.js");
-  }
-
-  let promiseArray = [];
+  includeScripts();
 
   DBHelper.getDB();
 
   var syncUserChanges = function(calendarId) {
-    debugger;
     DBHelper.crdtDBPromise.then(function(db) {
       var index = db
         .transaction(`participants-${calendarId}`)
@@ -86,76 +76,86 @@ function pushUserChangesToTheServer() {
       return index
         .getAll()
         .then(function(objects) {
-          DBHelper.crdtDBPromise
-            .then(function(db) {
-              if (!db) return;
-              var index = db
-                .transaction(`participants-${calendarId}`)
-                .objectStore(`participants-${calendarId}`);
-              return index.get(0).then(function(timestamp) {
-                debugger;
-                if (objects) {
-                  objects.forEach(object => {
-                    debugger;
-                    if (object.operations && object.operations.length > 0) {
-                      fetch(`${DBHelper.SERVER_URL}/api/${calendarId}/sync`, {
-                        method: "POST",
-                        body: JSON.stringify({
-                          lastCommitTimestamp: timestamp
-                            ? timestamp
-                            : undefined,
-                          updates: object.operations
-                        }),
-                        headers: {
-                          "Content-Type": "application/json; charset=utf-8"
-                        }
-                      });
-                    }
-                  });
-                }
-              });
-            })
-            .catch(function() {
-              // TODO throw an error
-            });
-        })
-        .then(function() {
-          return Promise.all(promiseArray)
-            .then(function() {
-              DBHelper.crdtDBPromise.then(function(db) {
-                if (!db) return;
-
-                var index = db
-                  .transaction(`participants-${calendarId}`)
-                  .objectStore(`participants-${calendarId}`);
-
-                return index.getAll().then(function(objects) {
-                  var tx = db.transaction(
-                    `participants-${calendarId}`,
-                    "readwrite"
-                  );
-                  var store = tx.objectStore(`participants-${calendarId}`);
-
-                  if (objects) {
-                    objects.forEach(object => {
-                      var temp = object;
-                      Object.setPrototypeOf(temp, SetCRDT.prototype);
-                      temp.processSentOperations();
-                      store.put(temp);
+          DBHelper.crdtDBPromise.then(function(db) {
+            if (!db) return;
+            var index = db
+              .transaction(`participants-${calendarId}`)
+              .objectStore(`participants-${calendarId}`);
+            return index.get(0).then(function(timestamp) {
+              if (objects) {
+                objects.forEach(object => {
+                  if (object.operations && object.operations.length > 0) {
+                    fetch(`${DBHelper.SERVER_URL}/api/${calendarId}/sync`, {
+                      method: "POST",
+                      body: JSON.stringify({
+                        lastCommitTimestamp: timestamp ? timestamp : undefined,
+                        updates: object.operations
+                      }),
+                      headers: {
+                        "Content-Type": "application/json; charset=utf-8"
+                      }
                     });
                   }
-                  console.log("Success! Promise all");
-                  return tx.complete;
                 });
-              });
-            })
-            .catch(function(error) {
-              throw "Silenced Exception! " + error;
+              }
             });
+          });
+        })
+        .then(function() {
+          return DBHelper.crdtDBPromise.then(function(db) {
+            if (!db) return;
+
+            var index = db
+              .transaction(`participants-${calendarId}`)
+              .objectStore(`participants-${calendarId}`);
+
+            return index.getAll().then(function(objects) {
+              var tx = db.transaction(
+                `participants-${calendarId}`,
+                "readwrite"
+              );
+              var store = tx.objectStore(`participants-${calendarId}`);
+
+              if (objects) {
+                objects.forEach(object => {
+                  if (object.type === "set") {
+                    var temp = object;
+                    Object.setPrototypeOf(temp, SetCRDT.prototype);
+                    temp.processSentOperations();
+                    store.put(temp);
+                  }
+                });
+              }
+              console.log("Success! Promise all");
+              return tx.complete;
+            });
+          });
         });
     });
   };
 
   syncUserChanges(1);
   syncUserChanges(2);
+}
+
+function includeScripts() {
+  if (typeof idb === "undefined") {
+    self.importScripts("js/idb.js");
+  }
+
+  if (typeof DBHelper === "undefined") {
+    self.importScripts("js/dbhelper.js");
+  }
+
+  if (typeof CounterCRDT === "undefined") {
+    self.importScripts("js/CRDTs/CounterCRDT.js");
+  }
+
+  if (typeof SetCRDT === "undefined") {
+    self.importScripts("js/CRDTs/SetCRDT.js");
+  }
+
+  if (typeof MVRegisterCRDT === "undefined") {
+    self.importScripts("js/CRDTs/MVRegisterCRDT.js");
+  }
 }
